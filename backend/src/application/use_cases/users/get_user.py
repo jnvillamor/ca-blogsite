f4509d73735aa17127c6ff64a37531496d713722
-m <villamorjohnnoel@gmail.com>
2026-01-14 20:11:07 +0800

    feat(be): implement use case for fetching users.

---
+++ backend/src/application/use_cases/users/get_user.py
@@ -0,0 +1,31 @@
+from src.application.dto import UserResponseDTO, PaginationDTO
+from src.application.repositories import IUserRepository
+from typing import Tuple, List
+
+class GetUserUseCase:
+  def __init__(self, user_repository: IUserRepository):
+    self.user_repository = user_repository
+    
+  def get_by_id(self, user_id: str) -> UserResponseDTO | None:
+    user = self.user_repository.get_user_by_id(user_id)
+    if not user:
+      return None
+
+    return UserResponseDTO.model_validate(user.to_dict())
+  
+  def get_by_username(self, username: str) -> UserResponseDTO | None:
+    user = self.user_repository.get_user_by_username(username)
+    if not user:
+      return None
+    
+    return UserResponseDTO.model_validate(user.to_dict())
+  
+  def get_all_users(self, pagination: PaginationDTO) -> Tuple[List[UserResponseDTO], int]:
+    users, count = self.user_repository.get_all_users(
+      skip=pagination.skip,
+      limit=pagination.limit,
+      search=pagination.search
+    )
+
+    user_dtos = [UserResponseDTO.model_validate(user.to_dict()) for user in users]
+    return user_dtos, count

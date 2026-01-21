3d2f24ec0f25228830a212362a2cee4976ed879f
-m <villamorjohnnoel@gmail.com>
2026-01-21 19:54:27 +0800

    fix(be): wrap the use case inside the unit of work.

--- backend/src/application/use_cases/users/delete_user.py
+++ backend/src/application/use_cases/users/delete_user.py
@@ -6,9 +6,10 @@ class DeleteUserUseCase:
     self.unit_of_work = unit_of_work
     
   def execute(self, user_id: str) -> None: 
-    user = self.unit_of_work.users.get_user_by_id(user_id)
-    
-    if not user:
-      raise NotFoundException("User", f"user_id: {user_id}")
-    
-    self.unit_of_work.users.delete_user(user_id)
+    with self.unit_of_work:
+      user = self.unit_of_work.users.get_user_by_id(user_id)
+      
+      if not user:
+        raise NotFoundException("User", f"user_id: {user_id}")
+      
+      self.unit_of_work.users.delete_user(user_id)

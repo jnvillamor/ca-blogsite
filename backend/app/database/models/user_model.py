9e51649a7c90dea742ca5df2ccd5a6f848bf6736
jnvillamor <jnvillamor@careerteam.com>
2026-02-05 17:22:29 +0800

    fix(be): migrate to postgre sql database.

--- backend/app/database/models/user_model.py
+++ backend/app/database/models/user_model.py
@@ -1,16 +1,10 @@
 from app.database.db import Base
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String, DateTime, CheckConstraint, func
+from sqlalchemy import String, DateTime, func
 from typing import Optional
 
 class UserModel(Base):
   __tablename__ = 'users'
-
-  __table_args__ = (
-    CheckConstraint("3 <= length(username) <= 20", name="username_length_check"),
-    CheckConstraint("2 <= length(first_name) <= 30", name="first_name_length_check"),
-    CheckConstraint("2 <= length(last_name) <= 30", name="last_name_length_check"),
-  )
 
   id: Mapped[str] = mapped_column(primary_key=True)
   first_name: Mapped[str] = mapped_column(String(30), nullable=False)

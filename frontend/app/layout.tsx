99b4654c5dfadf964d8ad5840034c382d878538e
jnvillamor <jnvillamor@careerteam.com>
2026-03-21 02:59:59 +0800

    fix(fe): rename files.

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import "@/styles/globals.css"
 import { ThemeProvider } from "@/components/providers/theme-provider"
-import NavBar from "@/components/NavBar"
+import NavBar from "@/components/navbar"
 import SessionProvider from "@/components/providers/session-provider"
 import { Toaster } from "sonner"
 
--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import Link from "next/link"
-import ThemeToggle from "./ThemeToggle"
+import ThemeToggle from "./theme-toggle"
 import { signOut, useSession } from "next-auth/react"
 import { Button } from "./ui/button"
 import {

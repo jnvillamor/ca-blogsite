460c6fb203b40c97826ffdc0d95b57c972eea3f7
jnvillamor <jnvillamor@careerteam.com>
2026-03-25 17:49:25 +0800

    fix(fe): fix styling and remove form validation.

--- frontend/app/blogs/_components/create-blog-form.tsx
+++ frontend/app/blogs/_components/create-blog-form.tsx
@@ -1,10 +1,6 @@
 "use client"
 
 import { Separator } from "@/components/ui/separator"
-import {
-  CreateBlogSchema,
-  CreateBlogData,
-} from "@/data-access/schemas/blogs.schema"
 import { useAppForm } from "@/hooks/form"
 
 const CreateBlogForm = () => {
@@ -13,51 +9,38 @@ const CreateBlogForm = () => {
       title: "",
       content: "",
     },
-    validators: {
-      onSubmit: () => CreateBlogSchema,
-    },
-    onSubmit: async ({ value }: { value: CreateBlogData }) => {
-      try {
-        const blogData = CreateBlogSchema.parse(value)
-        console.log(blogData)
-      } catch (error) {
-        console.error("Blog creation failed:", error)
-      }
-    },
   })
 
   return (
-    <div className="flex flex-col px-8 md:py-10 w-full">
-      <form
-        onSubmit={(e) => {
-          e.preventDefault()
-          form.handleSubmit()
-        }}
-        className="space-y-6"
-      >
-        <div className="space-y-2">
-          <form.AppField
-            name="title"
-            children={(field) => (
-              <field.CustomTextArea
-                placeholder="Untitled"
-                className="focus:outline-none border-none bg-transparent dark:bg-transparent resize-none text-3xl md:text-5xl font-bold overflow-hidden w-full px-0 focus-visible:ring-0"
-              />
-            )}
-          />
-          <p className="text-foreground/50">@username</p>
-        </div>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault()
+        form.handleSubmit()
+      }}
+      className="space-y-6"
+    >
+      <div className="space-y-2">
+        <form.AppField
+          name="title"
+          children={(field) => (
+            <field.CustomTextArea
+              placeholder="Untitled"
+              className="focus:outline-none border-none bg-transparent dark:bg-transparent resize-none text-3xl md:text-5xl font-bold overflow-hidden w-full px-0 focus-visible:ring-0"
+            />
+          )}
+        />
+        <p className="text-foreground/50">@username</p>
+      </div>
 
-        <Separator />
+      <Separator />
 
-        <div className="min-h-125">
-          <form.AppField
-            name="content"
-            children={(field) => <field.RichTextEditor />}
-          />
-        </div>
-      </form>
-    </div>
+      <div className="min-h-125">
+        <form.AppField
+          name="content"
+          children={(field) => <field.RichTextEditor />}
+        />
+      </div>
+    </form>
   )
 }
 
